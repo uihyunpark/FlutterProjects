f13bf92a4bdc324293da77930e8827d0ed606102
UiHyunPark <51102483+UiHyunPark@users.noreply.github.com>
2021-09-23 00:18:51 +0900

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Day1-Flutter 프로젝트생성'),
     );
   }
 }
